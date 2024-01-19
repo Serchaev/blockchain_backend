101f81c446c690a23b9dec2dce5fc1ca6c1c619b
Serchaev <german.serchaevk2.k2@gmail.com>
2024-01-19 19:54:37 +0300

    create database_base and database_factory

--- app/core/config.py
+++ app/core/config.py
@@ -1,20 +1,38 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent.parent
 
 
 class Setting(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../../.env")
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR.joinpath(".env"),
+    )
     DB_USER: str
     DB_PASS: str
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
+    MODE: str
 
     @property
-    def db_url(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    def db_url(self) -> str:
+        if self.MODE == "DEV":
+            return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/dev_{self.DB_NAME}"
+        if self.MODE == "TEST":
+            return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/test_{self.DB_NAME}"
+        if self.MODE == "PROD":
+            return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def db_echo(self) -> bool:
+        if self.MODE == "PROD":
+            return False
+        return True
 
 
 settings = Setting()
 
 if __name__ == "__main__":
-    print(settings.db_url)
+    print(BASE_DIR)
---
+++ app/core/database.py
@@ -0,0 +1,65 @@
+from asyncio import current_task
+
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    declared_attr,
+)
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
+
+from app.core.config import settings
+
+
+class DatabaseFactory:
+    def __init__(
+        self,
+        db_url: str,
+        db_echo: bool = False,
+    ):
+        self.engine = create_async_engine(
+            url=db_url,
+            echo=db_echo,
+        )
+
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_depends(self):
+        session = self.get_scoped_session()
+        try:
+            async with session() as s:
+                yield s
+        finally:
+            await session.remove()
+
+
+db_factory = DatabaseFactory(
+    db_url=settings.db_url,
+    db_echo=settings.db_echo,
+)
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(self) -> str:
+        return f"{self.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
