45d6edd785953632a48ec802881654f5ce0ed922
Serchaev <german.serchaevk2.k2@gmail.com>
2024-01-27 16:16:45 +0300

    feature: init alembic

--- app/main.py
+++ app/main.py
@@ -3,5 +3,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=25565, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=25565)
